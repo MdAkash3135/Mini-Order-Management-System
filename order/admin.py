3065b0d401d75502c6391cf7e297bbd111cbb1f4
MdAkash3135 <mdeaqubmia@gmail.com>
2025-08-08 18:01:43 +0600

    Register the models in admin

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Customer, Variant, Order, OrderItem
+
+admin.site.register(Customer)
+admin.site.register(Variant)
+admin.site.register(Order)
+admin.site.register(OrderItem)
